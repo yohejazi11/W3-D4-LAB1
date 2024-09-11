97678a8e21b6ae92bbfeb3c1b9869a37aa4f9ccd
yohejazi11 <yohejazi11@gmail.com>
2024-09-11 11:55:10 +0300

    add project

---
+++ main.js
@@ -0,0 +1,39 @@
+const intro=document.getElementsByClassName("welcomee")[0];
+intro.style.backgroundColor="grey";
+
+const parag=document.getElementById("par");
+
+parag.style.fontWeight=900;
+
+parag.setAttribute('class','paragraph');
+
+const imag=document.getElementsByTagName("img")[0];
+imag.setAttribute('src','https://images.pexels.com/photos/27676989/pexels-photo-27676989/free-photo-of-a-street-sign-with-a-picture-of-a-person-on-it.jpeg?auto=compress&cs=tinysrgb&w=600')
+
+
+const list=document.querySelector('ul');
+list.style.display="flex";
+list.style.gap="5rem";
+
+for(let i=0;i<3;i++){
+
+    const li=document.createElement('li');
+    li.setAttribute('class','li-class');
+    li.style.color="red"
+    li.style.border="1px solid black";
+    li.textContent=`item  ${i}`;
+    list.appendChild(li)
+}
+
+const bodyTabe=document.getElementsByTagName('tbody');
+const ttable=document.getElementsByTagName('table');
+ttable.setAttribute('class','table1');
+for(let i=0;i<2;i++){
+    const row=document.createElement('tr');
+    for(let j=0;j<3;j++){
+        const td=document.createElement('td');
+        td.textContent=`td nuumber ${i}`
+        row.appendChild(td);
+    }
+    bodyTabe.appendChild(row)
+}
